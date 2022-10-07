0726c39b29305c148357a3e3aff029ceabb2d8af
İsmail Yazıcı <i.yazicigircin18@gmail.com>
2022-10-07 07:18:42 +0300

    project is finished

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const taxRate = 0.18;
 const shippingPrice = 15;
-const shippingFreePrice = 200;
+const shippingFreePrice = 500;
 window.addEventListener("load", () => {
   localStorage.setItem("taxRate", taxRate);
   localStorage.setItem("shippingPrice", shippingPrice);
   localStorage.setItem("shippingFreePrice", shippingFreePrice);
 
-  sessionStorage.setItem("taxRate", taxRate);
-  sessionStorage.setItem("shippingPrice", shippingPrice);
-  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
+  //   sessionStorage.setItem("taxRate", taxRate);
+  //   sessionStorage.setItem("shippingPrice", shippingPrice);
+  //   sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
 
 const productsDiv = document.querySelector(".products");
@@ -38,9 +38,35 @@ productsDiv.addEventListener("click", (event) => {
 const calculateProductPrice = (button) => {
   const productInfoDiv = button.parentElement.parentElement;
   const price = productInfoDiv.querySelector(".product-price strong").innerText;
-  const adet = productInfoDiv.querySelector(".quantity").innerText;
-  const toplamÜrünFiyatı = productInfoDiv.querySelector(".product-line-price");
-  toplamÜrünFiyatı.innerText = (ürünFiyatı * adet).toFixed(2);
+  const quantity = productInfoDiv.querySelector(".quantity").innerText;
+  const productTotalDiv = productInfoDiv.querySelector(".product-line-price");
+  productTotalDiv.innerText = (price * quantity).toFixed(2);
 };
 
-const calculateCartPrice = () => {};
+const calculateCartPrice = () => {
+  const productTotalPricesDivs = document.querySelectorAll(
+    ".product-line-price"
+  );
+  let subtotal = 0;
+  productTotalPricesDivs.forEach((div) => {
+    subtotal += parseFloat(div.innerText);
+  });
+  const taxPrice = subtotal * localStorage.getItem("taxRate");
+  const shippingPrice = parseFloat(
+    subtotal > 0 && subtotal < localStorage.getItem("shippingFreePrice")
+      ? localStorage.getItem("shippingPrice")
+      : 0
+  );
+
+  document.querySelector("#cart-subtotal").lastElementChild.innerText =
+    subtotal.toFixed(2);
+  document.querySelector("#cart-tax p:nth-child(2)").innerText =
+    taxPrice.toFixed(2);
+  document.querySelector("#cart-shipping").children[1].innerText =
+    shippingPrice.toFixed(2);
+  document.querySelector("#cart-total").lastElementChild.innerText = (
+    subtotal +
+    taxPrice +
+    shippingPrice
+  ).toFixed(2);
+};
